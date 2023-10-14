764962bafa7786ba01e657b728be4bfb14497655
KaterynaMykytko <ekaterina.kaliazina@gmail.com>
2023-10-14 15:07:36 +0200

    Added changes in code.

--- src/org/courses/ex3_4/Main.java
+++ src/org/courses/ex3_4/Main.java
@@ -7,28 +7,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введіть ключ pro або exp");
         String key = scanner.next();
+        DocumentWorker documentWorker;
 
         switch (key) {
             case "pro": {
-                DocumentWorker proDocumentWorker = new ProDocumentWorker();
-                proDocumentWorker.openDocument();
-                proDocumentWorker.editDocument();
-                proDocumentWorker.saveDocument();
+                documentWorker  = new ProDocumentWorker();
                 break;
             }
             case "exp":{
-                DocumentWorker expertDocumentWorker = new ExpertDocumentWorker();
-                expertDocumentWorker.openDocument();
-                expertDocumentWorker.editDocument();
-                expertDocumentWorker.saveDocument();
+                documentWorker = new ExpertDocumentWorker();
                 break;
             }
             default:
-                DocumentWorker documentWorker = new DocumentWorker();
-                documentWorker.openDocument();
-                documentWorker.editDocument();
-                documentWorker.saveDocument();
+                documentWorker = new DocumentWorker();
                 break;
         }
+        documentWorker.openDocument();
+        documentWorker.editDocument();
+        documentWorker.saveDocument();
     }
 }
